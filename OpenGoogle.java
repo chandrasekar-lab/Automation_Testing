3ba8b1b7ce3f5582bbb8cfd8a44847f0e59266e9
chandrasekar-lab <54437874+chandrasekar-lab@users.noreply.github.com>
2021-10-27 18:41:19 +0530

    Initial commit

---
+++ OpenGoogle.java
@@ -0,0 +1,19 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGoogle {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		//1.Open Chrome
+		//2.Google home paage
+		
+		System.setProperty("Webdriver.chrome.driver", "C:\\Users\\chandrasekar\\Downloads\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver()
+	}
+
+}
